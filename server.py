4b3dce7d9b1b453d5cd34fad4f580a6338014cf2
Eugene <jekamen21@gmail.com>
2024-12-20 20:40:42 +0200

    New

---
+++ server.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=5000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Сервер запущен на порту {port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
